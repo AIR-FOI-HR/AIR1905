7e441652b9ee7407705aea5d26a01a8c699c19b0
Mihael Anđel <mandel@foi.hr>
2019-12-10 11:29:09 +0100

    #30 Prikaz GameScreenActivity preko cijelog ekrana (fullscreen)

--- HerbertGame/app/src/main/java/com/example/herbertgame/GameScreenActivity.java
+++ HerbertGame/app/src/main/java/com/example/herbertgame/GameScreenActivity.java
@@ -2,6 +2,8 @@ package com.example.herbertgame;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +12,8 @@ public class GameScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
     }
 }
