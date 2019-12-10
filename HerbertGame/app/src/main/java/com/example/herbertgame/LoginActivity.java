20ddc336ef12aa1d6790b84ea0e4b1702ecacfe3
dkoprek <45947197+dkoprek@users.noreply.github.com>
2019-12-10 19:43:34 +0100

    #1 Implementacija prijave s Google računom
    U klasi LoginActivity implementirani onClickListener za gumb kao i metode onActivityResult i handleSignInResult.
    U slučaju ispravne prijave otvara se GoogleSignOutActivity.
    Stvoren GoogleSignOutActivity s pripadajućim xml-om. GoogleSignOutActivity prikazuje podatke prijavljenog korisnika i sadrži metodu (i gumb) za odjavljivanje

--- HerbertGame/app/src/main/java/com/example/herbertgame/LoginActivity.java
+++ HerbertGame/app/src/main/java/com/example/herbertgame/LoginActivity.java
@@ -32,9 +32,65 @@ public class LoginActivity extends AppCompatActivity {
         //ako vraća null nije ulogiran, ako vraća GoogleSignInAccount object već je ulogiran
     }
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity_google);
+
         signInButton = findViewById(R.id.google_sign_in_button);
+
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.google_sign_in_button:
+                        signIn();
+                        break;
+                }
+            }
+        });
     }
+
+    private void signIn(){
+        Intent googleSignInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(googleSignInIntent,RC_SIGN_IN);
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            //pokreće GoogleSignOutActivity;  POTREBNO PROMIJENITI u neki drugi kasnije!!!
+            Intent intent = new Intent(LoginActivity.this, GoogleSignOutActivity.class);
+            startActivity(intent);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("Error", "signInResult:failed code=" + e.getStatusCode());
+        }
+    }
+}
+
