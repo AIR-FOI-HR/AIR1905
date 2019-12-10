9a05f7be8b593f6003d3c3aaf705341c85b4435e
Mihael Anđel <mandel@foi.hr>
2019-12-10 10:45:35 +0100

    Osnove Fragment klase za prikaz grafike igre
    U onCreateView metodi, umjesto Inflater klase, stvara se objekt prije izrađene GameView klase kako bi se prikazalo sučelje. Ne koristi se xml layout datoteka jer se radi o posebnoj grafici igre.

--- HerbertGame/app/src/main/java/com/example/herbertgame/fragments/GameDisplayFragment.java
+++ HerbertGame/app/src/main/java/com/example/herbertgame/fragments/GameDisplayFragment.java
@@ -1,6 +1,26 @@
 package com.example.herbertgame.fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.herbertgame.GameView;
+
 public class GameDisplayFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return new GameView(container.getContext());
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
 }
