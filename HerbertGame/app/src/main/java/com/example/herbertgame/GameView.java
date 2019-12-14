a7abf1725b2b5f70424bced62767463147ac6d01
Mihael Anđel <mandel@foi.hr>
2019-12-14 12:16:36 +0100

    #40 Redner grafike na ekranu premješten u drugu dretvu
    GameView klasa implementira Runnable interface kako bi se metode za crtanje grafike na ekran mogle odvijati na posebnoj dretvi. Dodana je i jednostavna animacije slike za testiranje.
    
    - holder.addCallback maknut je jer nema potrebe za njime
    - dodana je zastavica koja predstavlja stanje igranja igre
    - pause metoda postavlja zastavicu na false i pokušava zatvoriti dretvu
    - resume metoda postavlja zastavicu na true i stvara novu dretvu za prikaz grafike
    - run metoda u sebi privremeno sadrži svu logiku za crtanje grafike na ekran

--- HerbertGame/app/src/main/java/com/example/herbertgame/GameView.java
+++ HerbertGame/app/src/main/java/com/example/herbertgame/GameView.java
@@ -1,51 +1,74 @@
 package com.example.herbertgame;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Color;
+
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-public class GameView extends SurfaceView {
+
+public class GameView extends SurfaceView implements Runnable {
 
     private Bitmap bmp;
     private SurfaceHolder holder;
+    volatile boolean playing;
+    Thread gameThread = null;
 
     public GameView(Context context) {
         super(context);
         holder = getHolder();
-
-        holder.addCallback(new SurfaceHolder.Callback() {
-            @SuppressLint("WrongCall")
-            @Override
-            public void surfaceCreated(SurfaceHolder holder) {
-                Canvas c = holder.lockCanvas();
-                onDraw(c);
-                holder.unlockCanvasAndPost(c);
-            }
-
-            @Override
-            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
-            }
-
-            @Override
-            public void surfaceDestroyed(SurfaceHolder holder) {
-
-            }
-        });
-
         bmp = BitmapFactory.decodeResource(getResources(), R.drawable.dog);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
-        canvas.drawColor(Color.rgb(100, 200, 100));
-        canvas.drawBitmap(bmp, 10, 10, null);
     }
+
+    @Override
+    public void run() {
+        float xOffset = 1;
+        float yOffset = 1;
+        float yDirection = 5;
+        float xDirection = 6;
+        while(playing){
+            if(!holder.getSurface().isValid())
+                continue;
+            else{
+                Canvas canvas = holder.lockCanvas();
+                canvas.drawRGB(100, 200 ,100);
+                canvas.drawBitmap(bmp, xOffset, yOffset, null);
+                if(yOffset + bmp.getHeight() >= canvas.getHeight() || yOffset < 0){
+                    yDirection *= -1;
+                }
+                if(xOffset + bmp.getWidth() >= canvas.getWidth() || xOffset < 0){
+                    xDirection *= -1;
+                }
+                holder.unlockCanvasAndPost(canvas);
+                yOffset += yDirection;
+                xOffset += xDirection;
+            }
+        }
+    }
+
+    public void pause(){
+        playing = false;
+        try{
+            gameThread.join();
+            return;
+        } catch (InterruptedException e){
+
+        }
+    }
+
+    public void resume(){
+        playing = true;
+        gameThread = new Thread(this);
+        gameThread.start();
+
+    }
+
+
 }
