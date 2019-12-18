94c46103c58d4b77009ab2ca432df5a8a6b10ace
dkoprek <45947197+dkoprek@users.noreply.github.com>
2019-12-18 18:43:32 +0100

    #22 Merge branch 'PrijavaFacebook' into Development

--- HerbertGame/app/src/main/java/com/example/herbertgame/MainActivity.java
+++ HerbertGame/app/src/main/java/com/example/herbertgame/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.herbertgame;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    Button login;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        login = findViewById(R.id.go_to_login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
+
 }
