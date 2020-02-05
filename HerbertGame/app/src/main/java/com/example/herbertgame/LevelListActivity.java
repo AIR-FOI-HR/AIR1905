f4e35e68b36533639db046b4bff96d641bd3b6d1
dkoprek <45947197+dkoprek@users.noreply.github.com>
2020-02-05 20:36:04 +0100

    #53 Implementirana metoda getWorldRecords
    Dodana metoda getWorldRecords u klasu LevelListActivity koja dohvaća JSON s najboljim rezultatima po levelu.

--- HerbertGame/app/src/main/java/com/example/herbertgame/LevelListActivity.java
+++ HerbertGame/app/src/main/java/com/example/herbertgame/LevelListActivity.java
@@ -15,13 +15,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class LevelListActivity extends AppCompatActivity {
 
+    private RequestQueue mQ;
+
     //variables
     private ArrayList<String> levelNames = new ArrayList<>();
+    private ArrayList<String> worldRecords = new ArrayList<>();
     //missing worldRecords, personalBests and levelImages
 
     @Override
@@ -29,7 +43,16 @@ public class LevelListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.level_list_activity);
 
+        mQ = Volley.newRequestQueue(this);
+
         this.getLevelNames();
+        this.getWorldRecords();
+
+        for (String s:worldRecords
+             ) {
+            Log.d("REKORDI", s);
+        }
+
         this.initLevelRecyclerView();
 
     }
@@ -48,6 +71,34 @@ public class LevelListActivity extends AppCompatActivity {
             String[] levelNamesList = level.split("\\.");
             levelNames.add(levelNamesList[0]);
         }
+    }
+
+
+    private void getWorldRecords(){
+        String url = "http://cortex.foi.hr/air_herbert/getBestResultForEachLevel.php";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("levelList");
+                            for(int i = 0; i<jsonArray.length(); i++){
+                                JSONObject level = jsonArray.getJSONObject(i);
+
+                                Log.d("REKORD", level.getString("best_result"));
+                                worldRecords.add(level.getString("best_result"));
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        mQ.add(request);
     }
 
     private void initLevelRecyclerView(){
